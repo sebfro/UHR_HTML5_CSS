059c4bc2451b6187b63d6250610d36da21c24126
sebfro <sebastian17pepp@gmail.com>
2018-05-22 03:55:18 +0200

    Initial commit

---
+++ ACOS_script.js
@@ -0,0 +1,36 @@
+function switchToMobileNavBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+var slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-red", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-red";
+}
---
+++ Working Examples/Working footer and slideshow with text next to it and a round button with arrow icon/test2.js
@@ -0,0 +1,23 @@
+var i = 0;
+var slideShowImages = new Array();
+
+// LIST OF IMAGES
+slideShowImages[0] = "SlideShow_1.jpg";
+slideShowImages[1] = "SlideShow_2.jpg";
+slideShowImages[2] = "SlideShow_3.jpg";
+slideShowImages[3] = "SlideShow_4.jpg";
+
+function swapImage(n)
+{
+
+    i += n;
+    if(i > slideShowImages.length -1){
+        i = 0;
+    } else if (i < 0){
+        i = slideShowImages.length -1;
+    }
+    document.getElementById("slideCounter").innerText = i+1 + "/4";
+    document.slide.src = slideShowImages[i];
+
+    //setTimeout("swapImage()",3000);
+}
